ef13e087a71034327666d0019ebe1be946b9247c
jzpacheco <jeffersonpacheco@hotmail.com>
2026-04-03 01:00:14 -0300

    feat: new routes

--- main.go
+++ main.go
@@ -8,6 +8,13 @@ import (
 func main() {
 
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		header := w.Header()
+		header.Add("content-type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
 	server := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
